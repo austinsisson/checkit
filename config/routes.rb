2652418dc08b0a495ae6b32802ceeb8839caf94e
Austin Sisson <austinsisson@gmail.com>
2014-11-05 18:35:32 -0500

    created welcome controller, added devise gem, created welcome and about pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :lists, only: [:new, :create, :show]
   
   
+  get 'about' => 'welcome#about'
+  
   root to: 'welcome#index'
 end
--- spec/features/project_manager_creates_todo_spec.rb
+++ spec/features/project_manager_creates_todo_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 feature 'Project manager creates to-do list' do
   scenario 'Succesfully' do
     visit new_list_path
-    fill_in 'What do you need to do?', with: 'Meet up with the team'
+    fill_in 'Description', with: 'Meet up with the team'
     click_button 'Save'
       expect( page ).to have_content('Your new to-do was saved!')
       expect( page ).to have_content('Meet up with the team')
